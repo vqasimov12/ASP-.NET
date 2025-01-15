d9e606271a85aad5c5d883a7b6bd1c5e4e225c2f
Qasimov Vaqif Xalid <Qasim_wu20@itstep.edu.az>
2025-01-15 11:58:34 +0400

    lesson 2

---
+++ Lesson2_MWC/Lesson2_MWC/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson2_MWC.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Cityİd {  get; set; }
+    }
+}
