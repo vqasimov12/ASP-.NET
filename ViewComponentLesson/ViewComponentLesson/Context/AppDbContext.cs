9d627fe148aa133d6befe746a5b69875d41dfe33
Qasimov Vaqif Xalid <Qasim_wu20@itstep.edu.az>
2025-01-22 12:19:22 +0400

    lesson 5

---
+++ ViewComponentLesson/ViewComponentLesson/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ViewComponentLesson.Entities;
+
+namespace ViewComponentLesson.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category>Categories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
